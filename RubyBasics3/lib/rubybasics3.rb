a5e9876742defe27d179a3f84fb4be38e6d6f6d3
Jacob Ferragut <jferragu@uncc.edu>
2020-02-12 21:59:44 +0000

    Ruby OO Basics - Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -6,8 +6,14 @@ class BookInStock
     def initialize(isbn, price)
         @isbn = isbn
         @price = price
+        if isbn.length==0
+            raise ArgumentError.new('ISBN is empty string')
+        end
+        if price<=0
+            raise ArgumentError.new('Price is less than or equal to 0')
+        end
     end
     attr_accessor :isbn
     attr_accessor :price
-
+    
 end
